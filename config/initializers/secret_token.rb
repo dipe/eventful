4bdecf2786a6267d035bb533538e72cc042d37d3
Peter Ehrenberg <pe@dipe.de>
2010-10-13 18:47:05 +0200

    Freshly generated Rails 3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventful::Application.config.secret_token = 'f9467c54c78cf65900c2e747e878fc27df90b7ea8b5ec53fe840bf0973827e2cab81b7e8f103efd6713abd81d77d187486bd0d75671ad4ee085c88f283a064d9'
