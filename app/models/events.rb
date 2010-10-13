4461813b10b77bbeab22b1fe14e8ac058e460019
Peter Ehrenberg <pe@dipe.de>
2010-10-13 20:29:12 +0200

    Try to fix specs

--- app/models/events.rb
+++ app/models/events.rb
@@ -1,21 +1,21 @@
 class Events < CouchRest::Model::Base
   property :title, String
   property :message, String
-  property :created_at, Datetime
+  property :created_at, Time
   property :application, String
   property :environment, String
   property :controller, String
   property :action, String
-  property :request_url, Text
-  property :request_params, Text
+  property :request_url, String
+  property :request_params, String
   property :request_data_type, String
-  property :request_data, Text
+  property :request_data, String
   property :session_id, String
   property :session_data_type, String
-  property :session_data, Text
+  property :session_data, String
   property :additional_data_type, String
-  property :additional_data, Text
-  property :backtrace, Text
+  property :additional_data, String
+  property :backtrace, String
   property :node, String
   property :pid, String
 end
