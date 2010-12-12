f0f354be1a0a3e80c6c87e56b95a8a992312c2d3
Peter Ehrenberg <pe@dipe.de>
2010-12-12 16:20:46 +0100

    Refine show view; German translation founded

--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -2,55 +2,64 @@ require 'spec_helper'
 
 describe "events/show.html.erb" do
 
-  def mock_event(stubs={})
-    (@mock_event ||= mock_model(Event).as_null_object).tap do |event|
-      event.stub(stubs) unless stubs.empty?
-    end
-  end
-
   before(:each) do
+    @now = Time.now
     @event = assign(:event,
-                    mock_event(:id => 1,
-                               :title => "Title",
-                               :message => "Message",
-                               :application => "Application",
-                               :environment => "Environment",
-                               :version => "Version",
-                               :controller => "Controller",
-                               :action => "Action",
-                               :request_url => "MyText",
-                               :request_params => "MyText",
-                               :request_data_type => "Request Data Type",
-                               :request_data => "MyText",
-                               :session_id => "Session",
-                               :session_data_type => "Session Data Type",
-                               :session_data => "MyText",
-                               :additional_data_type => "Additional Data Type",
-                               :additional_data => "MyText",
-                               :backtrace => "MyText",
-                               :node => "Node",
-                               :pid => "Pid",
-                               :created_at => "value for created_at"
+                    stub_model(Event,
+                               :id => 1,
+                               :title => "value for title",
+                               :message => "value for message",
+                               :application => "value for application",
+                               :environment => "value for environment",
+                               :version => "value for version",
+                               :controller => "value for controller",
+                               :action => "value for action",
+                               :request_url => "value for request_url",
+                               :request_params => "value for request_params",
+                               :request_data_type => "value for request_data_type",
+                               :request_data => "value for request_data",
+                               :session_id => "value for session_id",
+                               :session_data_type => "value for session_data_type",
+                               :session_data => "value for session_data",
+                               :additional_data_type => "value for additional_data_type",
+                               :additional_data => "value for additional_data",
+                               :backtrace => "value for backtrace",
+                               :node => "value for node",
+                               :pid => "value for pid",
+                               :created_at => @now,
+                               :to_param => 'value for to_param'
                                ))
   end
 
-  it "renders attributes in <p>" do
+  it "renders attribute values" do
     render
-    rendered.should match(/Title/)
-    rendered.should match(/Message/)
-    rendered.should match(/Application/)
-    rendered.should match(/Environment/)
-    rendered.should match(/Version/)
-    rendered.should match(/Controller/)
-    rendered.should match(/Action/)
-    rendered.should match(/MyText/)
-    rendered.should match(/MyText/)
-    rendered.should match(/MyText/)
-    rendered.should match(/Session/)
-    rendered.should match(/MyText/)
-    rendered.should match(/MyText/)
-    rendered.should match(/MyText/)
-    rendered.should match(/Node/)
-    rendered.should match(/Pid/)
+    rendered.should match(/value for title/)
+    rendered.should match(/value for message/)
+    rendered.should match(/value for application/)
+    rendered.should match(/value for environment/)
+    rendered.should match(/value for version/)
+    rendered.should match(/value for controller/)
+    rendered.should match(/value for action/)
+    rendered.should match(/#{Regexp.escape(@now.to_s)}/)
+  end
+
+  describe "with empty values" do
+    before(:each) do
+      @event = assign(:event,
+                      stub_model(Event,
+                                 :id => 1,
+                                 :to_param => 'value for to_param'))
+    end
+    
+    it "not render attributes" do
+      render
+      rendered.should_not match(/value for title/)
+      rendered.should_not match(/value for message/)
+      rendered.should_not match(/value for application/)
+      rendered.should_not match(/value for environment/)
+      rendered.should_not match(/value for version/)
+      rendered.should_not match(/value for controller/)
+      rendered.should_not match(/value for action/)
+    end
   end
 end
